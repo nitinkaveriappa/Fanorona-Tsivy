df2d57a50672d182f7174dea803d3ccd1977ff34
nitinkaveriappa <nitinkaveriappa@gmail.com>
2016-09-16 18:26:48 -0400

    Update game_logic.php

--- game_logic.php
+++ game_logic.php
@@ -26,29 +26,29 @@ for($i=0;$i<45;$i++)
 	{
 		$new = $i;
 		if($s1[$i]!=0)
-		$ply = $s1[$i];
+		$ply1 = $s1[$i];
 	}
 	if(strcmp($s1[$i],$s2[$i]) > 0)
 	{
 		$old = $i;
 		if($s1[$i]!=0)
-		$ply = $s1[$i];
+		$ply2 = $s1[$i];
 	}
 	
 }
 $pos = $new - $old;
 
 echo "	new =$new <br/>
-		old = $old<br/>
-		pos= $pos <br/>
-		ply1=$ply1<br/>
-		ply2 =$ply2<br/>
-		old.= $s1<br/>
-		mov= $s2<br/>"; 
+	old = $old<br/>
+	pos= $pos <br/>
+	ply1=$ply1<br/>
+	ply2 =$ply2<br/>
+	old.= $s1<br/>
+	mov= $s2<br/>"; 
 
-if(($pos == 1 || $pos == -1 || $pos == 9 || $pos == -9 || $pos == 8 || $pos == -8 || $pos == 10 || $pos == -10) && (($ply1==0&&$ply2==1) || ($ply1==0&&$ply2==2) || ($ply1==1&&$ply2==0) || ($ply1==2&&$ply2==0)))
+if(($pos == 1 || $pos == -1 || $pos == 9 || $pos == -9 || $pos == 8 || $pos == -8 || $pos == 10 || $pos == -10) && (($ply1==0 && $ply2==1) || ($ply1==0 && $ply2==2) || ($ply1==1 && $ply2==0) || ($ply1==2 && $ply2==0)))
 {
-	for($i = $new+$pos;$i>=0&&$i<45;)
+	for($i = $new+$pos;$i>=0 && $i<45;)
 	{
 		if($s2[$i] != 0 && $s2[$i] != $ply2)
 		{
