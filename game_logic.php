b784b59fca5b9b00caa4db2764161573c3cd7569
nitinkaveriappa <nitinkaveriappa@gmail.com>
2016-09-19 01:25:29 -0400

    Update game_logic.php

--- game_logic.php
+++ game_logic.php
@@ -1,6 +1,10 @@
-
 <?php
-
+/**
+ * Created by PhpStorm.
+ * User: Paurav & Nitin
+ * Date: 9/18/2016
+ * Time: 10:09 PM
+ */
 $s1 = "111111111";
 $s1 .="111111111";
 $s1 .="121201212";
@@ -16,53 +20,109 @@ $s2 .="222222222";
 $pos=0;
 $new=0;
 $old=0;
-$ply1=0;
-$ply2=0;
+$player=0;
 
+//Checks to see if the win condition has been reached
+//If returns $flag=1 then win
+function check_win()
+{
+    global $s2, $player;
+    $flag=1;
+    for($i=0;$i<45;$i++)
+    {
+        if($s2[$i]!=0)
+        {
+            if($s2[$i]!=$player)
+            {
+                $flag=0;
+            }
+        }
+    }
+    return $flag;
+}
+
+// To read the change in the state
 for($i=0;$i<45;$i++)
 {
-	
-	if(strcmp($s1[$i],$s2[$i]) < 0)
-	{
-		$new = $i;
-		if($s1[$i]!=0)
-		$ply = $s1[$i];
-	}
-	if(strcmp($s1[$i],$s2[$i]) > 0)
-	{
-		$old = $i;
-		if($s1[$i]!=0)
-		$ply = $s1[$i];
-	}
-	
+
+    if($s1[$i] != $s2[$i])
+    {
+        if($s1[$i] < $s2[$i])
+        {
+            $new = $i;
+        }
+        else
+            $old = $i;
+    }
+
 }
 $pos = $new - $old;
+if($s2[$new]!=$s2[$old])
+    $player = $s2[$new]+$s2[$old];
 
-echo "	new =$new <br/>
-		old = $old<br/>
-		pos= $pos <br/>
-		ply1=$ply1<br/>
-		ply2 =$ply2<br/>
-		old.= $s1<br/>
-		mov= $s2<br/>"; 
-
-if(($pos == 1 || $pos == -1 || $pos == 9 || $pos == -9 || $pos == 8 || $pos == -8 || $pos == 10 || $pos == -10) && (($ply1==0&&$ply2==1) || ($ply1==0&&$ply2==2) || ($ply1==1&&$ply2==0) || ($ply1==2&&$ply2==0)))
+$validate = false;
+//To validate if legal move
+if(($old % 2) == 0)
 {
-	for($i = $new+$pos;$i>=0&&$i<45;)
-	{
-		if($s2[$i] != 0 && $s2[$i] != $ply2)
-		{
-			$s2[$i] = 0;
-		 	$i += $pos;
-		}
-		else 
-		break;
-	}
+    $valid = array("-9", "-1", "1", "9");
 }
-else
-{
-	echo "Invalid Move<br/>";
+else {
+    $valid = array("-10", "-9", "-8", "-1", "1", "8", "9", "10");
 }
 
-echo "curr= $s2";
+for($i=0;$i<sizeof($valid);$i++)
+{
+    if($pos == $valid[$i])
+    {
+        $validate = true;
+        echo "$valid[$i]<br/>";
+    }
+}
+// To validate if pawn has moved to a empty slot
+if($validate)
+{
+    if( $player > 0 && $player < 3)
+    {
+        $validate = true;
+        echo "valid move";
+    }
+    else
+    {
+        $validate = false;
+        echo "invalid move";
+    }
+}
+echo "	new = $new <br/>
+		old = $old <br/>
+		pos = $pos <br/>
+		player = $player <br/>
+		old = $s1 <br/>
+		mov = $s2 <br/>";
+//Removes the pawns related
+if($validate)
+{
+    for($i = $new+$pos;$i>=0&&$i<45;)
+    {
+        if($s2[$i] != 0 && $s2[$i] != $player)
+        {
+            $s2[$i] = 0;
+            $i += $pos;
+        }
+        else
+            break;
+    }
+    echo "curr= $s2";
+    if(check_win())
+    {
+        echo "Player $player Wins";
+    }
+}
+else {
+    //Checks if move is Not made at all
+    if ($new == 0 && $old == 0) {
+        echo "No Move Made";
+    } else {
+        echo "Invalid Move";
+    }
+}
 ?>
