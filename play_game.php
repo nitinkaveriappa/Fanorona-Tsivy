8e09b257f7d29b5b43cf950eabb48d607884786e
nitinkaveriappa <nitinkaveriappa@gmail.com>
2016-09-24 16:41:35 -0400

    play_game
    Still incomplete, needs additional input to be passed

---
+++ play_game.php
@@ -0,0 +1,37 @@
+<?php
+
+include 'game_logicv0.4.php';
+require_once('dbconnect.php');
+
+//Program requires New Node State, GameID, PlayerID as I/p
+
+$s2 = "111111111";
+$s2 .="111111111";
+$s2 .="121212212";
+$s2 .="222220222";
+$s2 .="222222222";
+
+$game_id=10000;
+$player_id=1000;
+
+
+$query = "SELECT node_list FROM fanodb.gm_st where game_id=:gameID AND player_id=:playerID ORDER BY move_count DESC LIMIT 1;";
+$oldNodeQuery = $connection->prepare($query); 
+$oldNodeQuery->bindParam(':gameID',$game_id);
+$oldNodeQuery->bindParam(':playerID',$player_id);
+$oldNodeQuery->execute();
+
+// set the resulting array to associative
+$result = $oldNodeQuery->fetch(PDO::FETCH_ASSOC);
+$s1=$result['node_list'];
+
+echo "s1=$s1<br/>";
+
+$start = new game_logic($s1,$s2);
+$start->run();
+
+
+	
+$connection = null;
+
+?>
