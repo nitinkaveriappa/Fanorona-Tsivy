de3622dbc763f2ad19c3287e979642bf89e3114d
nitinkaveriappa <nitinkaveriappa@gmail.com>
2016-09-24 12:29:24 -0400

    Add files via upload
    Added new database connection php file that takes care of secure db connection with mysql
    Also added config.ini file to store data in a secure manner

---
+++ dbconnect.php
@@ -0,0 +1,34 @@
+<?php
+
+function db_connect() {
+
+        // Define connection as a static variable, to avoid connecting more than once 
+    static $connection;
+
+        // Try and connect to the database, if a connection has not been established yet
+    if(!isset($connection)) {
+             // Load configuration as an array. Use the actual location of your configuration file
+        $config = parse_ini_file('config.ini'); 
+		$servername = $config['servername'];
+		$dbname = $config['dbname'];
+		
+		try {
+			$connection = new PDO("mysql:host=$servername;dbname=$dbname", $config['username'], $config['password']);
+			// set the PDO error mode to exception
+			$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			echo "Connected successfully"; 
+		}
+		// If connection was not successful, handle the error
+		catch(PDOException $e)
+		{
+			 // Handle error - notify administrator, log to a file, show an error screen, etc.
+			echo "Connection failed: " . $e->getMessage();
+		}
+    }
+    return $connection;
+}
+
+// Connect to the database
+$connection = db_connect();
+
+?>
