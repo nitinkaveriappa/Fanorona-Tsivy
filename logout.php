8038b2635f116f9e81bd5754103094487e52043f
nitinkaveriappa <nitinkaveriappa@gmail.com>
2016-11-02 00:35:34 -0400

    Logging

--- logout.php
+++ logout.php
@@ -1,23 +1,26 @@
 <?php
    session_start();
+   require_once('log.php');
   require_once('dbconnect.php');
   $playerId = $_SESSION['player_id'];
   	//Unset all the flags
 	$unsetFlagsQuery = $connection->prepare("UPDATE flg_ls SET login_flag=0, lobby_flag=0, ingame_flag=0 WHERE player_id=:playerId ;");
 	$unsetFlagsQuery->bindParam(':playerId',$playerId);
 	$unsetFlagsQuery->execute();
-	
+  log_it("$playerId logging out");
+
 	// Unset all of the session variables
 	$_SESSION = array();
 
 	// Delete the session cookie
-	if (ini_get("session.use_cookies")) 
+	if (ini_get("session.use_cookies"))
 	{
     	$params = session_get_cookie_params();
     	setcookie(session_name(), '', time() - 42000, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
 	}
 	//Destroy session
    	if(session_destroy()) {
+      log_it("$playerId logout successful");
       header("Location:index.html");
    }
 ?>
